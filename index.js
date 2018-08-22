fb2bafea11a7bfa78f4fa723dddccf2a2ad9b5ff
cryptoalgorythm <cryptoalgorythm@gmail.com>
2018-08-22 17:09:16 -0400

    testing heroku

--- index.js
+++ index.js
@@ -36,6 +36,12 @@ app.all('/*', (req, res, next) => {
     }
 })
 
+app.get('/', (req, res) => {
+    res.json({
+        Hello: "Hello World"
+    })
+})
+
 db.sequelize.sync().then(() => {
     http2
         .createServer(options, app)
